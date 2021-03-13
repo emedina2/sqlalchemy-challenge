abeb4449f0776ee538dac6a62564ae8d42f54030
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-03-12 21:39:12 -0600

    Added date format verification

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import sqlalchemy
+import datetime
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, distinct, desc
@@ -33,8 +34,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start/<end><br/>"
+        f"/api/v1.0/< start ><br/>"
+        f"/api/v1.0/< start >/< end ><br/>"
     )
 #Define precipitation page
 @app.route("/api/v1.0/precipitation")
@@ -54,6 +55,69 @@ def stations():
     session.close()
     return jsonify(total_stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("Tobs Page Opened")
+    session = Session(engine)
+    most_popular = "USC00519281"
+    station_temp= session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.date >= 2016-8-18).all()
+
+    return jsonify(station_temp)
+
+@app.route("/api/v1.0/<start>")
+def start_only (start):
+    print(f"Start date {start} entered.")
+    start_date = start
+ #Check if date format is correct   
+    try:
+        datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        correct_format = True
+    except ValueError:
+        correct_format = False
+
+    if correct_format == True:
+        session = Session(engine)
+        date_high = session.query(func.max(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        date_low = session.query(func.min(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        date_avg = session.query(func.avg(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        session.close()
+        json_data = {"Date": start, "Max Temp": date_high[0], "Min Temp": date_low[0], "Avg Temp": date_avg[0]}
+        return jsonify(json_data)
+    else:
+        return "No data for Date Entered or invalid date foorat. Date must be in format YYYY-MM-DD and between "
+    
+        
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end)
+    print(f'Start date {start} and end date {end} entered.')
+    start_date = start_date
+    end_date = end
+    try:
+        datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        correct_format = True
+    except ValueError:
+        correct_format = False
+
+    if correct_format == True:
+        session = Session(engine)
+        date_high = session.query(func.max(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        date_low = session.query(func.min(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        date_avg = session.query(func.avg(Measurement.tobs)).\
+            filter(Measurement.date == start_date).all()
+        session.close()
+        json_data = {"Date": start, "Max Temp": date_high[0], "Min Temp": date_low[0], "Avg Temp": date_avg[0]}
+        return jsonify(json_data)
+    else:
+        return "No data for Date Entered or invalid date foorat. Date must be in format YYYY-MM-DD and between "
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
